43e04c0026fbbc975d9b2e2d702a614c4be6218d
1AoB <78208268+1AoB@users.noreply.github.com>
2023-06-29 17:10:51 +0800

    Add files via upload

---
+++ 源码/stm32/project/Objects/lv_color.d
@@ -0,0 +1,5 @@
+.\objects\lv_color.o: ..\LVGL\lvgl-v6.0\src\lv_misc\lv_color.c
+.\objects\lv_color.o: ..\LVGL\lvgl-v6.0\src\lv_misc\lv_color.h
+.\objects\lv_color.o: ..\LVGL\lvgl-v6.0\src\lv_misc\../../../lv_conf.h
+.\objects\lv_color.o: D:\KEIL5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\lv_color.o: ..\LVGL\lvgl-v6.0\src\lv_misc\../../../lvgl-v6.0/src/lv_conf_checker.h
